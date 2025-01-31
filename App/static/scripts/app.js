4452f98cc79fbe1afec927277676169330ca9a30
AnSuChi <anutika02@gmail.com>
2025-01-31 17:53:10 +0100

    add favicon and minor d3 test

--- App/static/scripts/app.js
+++ App/static/scripts/app.js
@@ -1,2 +1,8 @@
 // Cache reset --> cmd+shift+R
 console.log("Hello world they say???");
+
+// Test to see that d3 works
+d3.select("#graph-nav-domain")
+  .append("p")
+  .text("Hello from D3!")
+  .style("color", "red");
